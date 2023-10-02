f010cbd0cbb9830606437a3f2983dec32d8db03e
JyonasMekonnen <146183089+JyonasMekonnen@users.noreply.github.com>
2023-10-01 21:53:00 -0400

    Add files via upload

---
+++ untitled folder 7/Backend/functions/util/config.js
@@ -0,0 +1,14 @@
+/**
+ * configuration of our firebase project
+ */
+
+module.exports = {
+    apiKey: "AIzaSyDenJlzVzL0LmDGNhZ6eH6TlZAmqFT4eJU",
+    authDomain: "twitter-clone-app.firebaseapp.com",
+    databaseURL: "https://twitter-clone-app.firebaseio.com",
+    projectId: "twitter-clone-app",
+    storageBucket: "twitter-clone-app.appspot.com",
+    messagingSenderId: "714072408637",
+    appId: "1:714072408637:web:eb709d07ebb91959a42054",
+    measurementId: "G-7B0TRRWSJW"
+}
